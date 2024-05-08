cc82f7b9592f3827dd00a970615fa3a1f0505779
mateuscamargo <mateuscamargolima@gmail.com>
2024-05-08 20:00:10 -0300

    Alguns casos de tipos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,25 @@
+import java.util.Arrays;
+
 public class TiposVariaveis {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws  Exception {
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+
+        numero = 2;
+
+        System.out.println(numero);
+        //valor_nunca_mudara ?
+        // variaveis vs constantes
+
+        final double VALOR_DE_PI = 3.14;
+
+        // declarando final torna a variavel constante e a representação é feita em caixa alta
+
+
     }
 }
