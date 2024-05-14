d21d92a92d1d4f98ccf90e438b2a8d5b012d1f59
mateuscamargo <mateuscamargolima@gmail.com>
2024-05-13 21:29:47 -0300

    Add Class ContaTerminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,6 +1,23 @@
-public class ContaTerminal {
-    public static void main(String[] args) {
+import java.util.Arrays;
+import java.util.Scanner;
 
-        System.out.println("Hello world!");
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception{
+        Scanner scanner = new Scanner(System.in);
+        String clienteNome;
+        String agencia;
+        int contabanco;
+        double saldo = 237.48;
+
+        System.out.println("Olá, para começar digite seu nome: ");
+        clienteNome = scanner.nextLine();
+
+        System.out.println("Digite sua agência: ");
+        agencia = scanner.nextLine();
+
+        System.out.println("Digite sua conta bancária: ");
+        contabanco = scanner.nextInt();
+
+        System.out.println("Olá " + clienteNome + ", obrigado por criar uma conta (" + contabanco + ") em nosso banco, sua agência é " + agencia + " e seu saldo " + saldo + " já está disponível para saque!");
     }
 }
